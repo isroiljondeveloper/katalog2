d38a8d1fdbd17e3827d9d97c718ad2fce2d9db09
isroiljondeveloper <isroiljonmusajonov01@gmail.com>
2025-08-13 19:14:17 +0500

    katalog2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         
           <div className="col-md-7 mb-2 footer-logo d-flex gap-4">
             <img src="./img/footer.logo.png" alt="Logo"/>
-          <div>
+          <div className="flex-wrap d-flex">
             <a href="#" className="me-3 text-dark text-decoration-none">О компании</a>
             <a href="#" className="me-3 text-dark text-decoration-none">Контакты</a>
             <a href="#" className="me-3 text-dark text-decoration-none">Вакансии</a>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,24 +6,24 @@ function Navbar() {
     <div className="container">
       <img src="./img/navbar.logo.png" alt=""/>
       <div className="d-flex gap-3 align-items-center">
-        <button className="butt p-2 px-3  d-md-flex d-none "><img src="./img/ikonka.png" alt=""/>Каталог</button>
+        <button className="butt p-2 px-3  d-xl-flex d-none "><img src="./img/ikonka.png" alt=""/>Каталог</button>
         <input  className="typ" type="text"  placeholder="Найти товар"/>
       </div>
       <div className="d-flex gap-4">
-        <div className="text-center flex-column d-md-flex d-none ">
+        <div className="text-center flex-column d-xl-flex d-none ">
           <img className="img-fluid ff" src="./img/ikon1.png" alt=""/>
           <p>Избранное</p>
         </div>
-        <div className="text-center flex-column d-md-flex d-none">
+        <div className="text-center flex-column d-xl-flex d-none">
           <img className="img-fluid ff" src="./img/ikon2.png" alt=""/>
           <p>Заказы</p>
         </div>
-        <div className="text-center flex-column d-md-flex d-none">
+        <div className="text-center flex-column d-xl-flex d-none">
           <img className="img-fluid ff" src="./img/ikonka3.png" alt=""/>
           <p>Корзина</p>
         </div>
       </div>
-      <div className="d-flex gap-3 align-items-center text-center d-md-flex d-none">
+      <div className="d-flex gap-3 align-items-center text-center d-xl-flex d-none">
         <img src="./img/puple.png" alt=""/>
         <p>Алексей</p>
         <img className="img-fluid" src="./img/ikonka4.png" alt=""/>
